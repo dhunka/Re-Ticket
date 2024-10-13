b174018cf6cd28c41ef8719ecb0b58349f81a3b0
M0nnx <devialfaroesteban@gmail.com>
2024-10-13 11:59:29 -0300

    fix: route.ts

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -3,28 +3,29 @@ import db from "@/libs/db";
 import bcrypt from 'bcrypt';
 
 export async function POST(request: NextRequest) {
-    try {
-        const data = await request.json();
-        console.log(data);
+  try {
+    const data = await request.json();
+    console.log(data);
 
-        const saltRounds = 10; 
-        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
-        console.log('Contraseña encriptada:', hashedPassword); 
-        const birthDate = new Date(data.fecha_de_nacimiento);
-        birthDate.setUTCHours(0, 0, 0, 0); 
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+    console.log('Contraseña encriptada:', hashedPassword);
+    const birthDate = new Date(data.fecha_de_nacimiento);
+    birthDate.setUTCHours(0, 0, 0, 0);
 
-        const newUser = await db.usuario.create({
-            data: {
-                ...data,
-                password: hashedPassword,
-                fecha_de_nacimiento: birthDate 
-            }
-        });
+    const newUser = await db.usuario.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+        fecha_de_nacimiento: birthDate
+      }
+    });
 
-        return NextResponse.json(newUser, { status: 201 });
-    } catch (error) {
-        console.error("Error al crear el usuario:", error);
-        return NextResponse.json({ error: "Error al crear el usuario" }, { status: 500 });
-    }
+
+    return NextResponse.json(newUser, { status: 201 });
+  } catch (error) {
+    console.error("Error al crear el usuario:", error);
+    return NextResponse.json({ error: "Error al crear el usuario" }, { status: 500 });
+  }
 }
 
