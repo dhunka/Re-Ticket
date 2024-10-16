439031f216922663cfe5e91224e95a396f78a5c4
Nicolas <nicolas.torres.2003@gmail.com>
2024-10-16 14:16:53 -0300

    Register y login concluidos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-function HomePage() {
+
+export default async function HomePage() {
   return (
     <section className="h-[calc(100vh-7rem)] flex justify-center 
     items-center">
         <div>
-            <h1 className="text-white text-5xl">HomePage</h1>
+            <h1 className="text-black text-5xl">Bienvenido :v</h1>
+            <p className=" mb-6">
+              En proceso....
+            </p>
         </div>
     </section>
 
   );
 }
-
-export default HomePage
