38918e3645f9dc2219e695a1a77f5ab2f37cfbdc
M0nnx <deviaalfaroesteban@gmail.com>
2024-11-05 21:48:52 -0300

    change: login and register with clerk

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -111,6 +111,7 @@ const RegisterPage = () => {
     }
   };
 
+  
   return (
     <div className="w-full max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
       <h1 className="text-2xl font-bold mb-6 text-center">Registro</h1>
