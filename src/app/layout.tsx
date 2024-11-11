9ed6bf491dab8aa7215efc8f7131d3608e08820d
M0nnx <deviaalfaroesteban@gmail.com>
2024-11-11 12:48:28 -0300

    Add: page entrada
    se añadio la page de entrada que nos permite ver en detalle cada entrada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
               <Header/>
           <main>
-              <div className="flex items-start justify-center">
+              <div>
               <div >{children}</div>
               </div>  
           </main>   
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/login(.*)', '/register(.*)','/sign-up(.*)','/'])
+const isPublicRoute = createRouteMatcher(['/login(.*)', '/register(.*)','/', '/Compra'])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)){
